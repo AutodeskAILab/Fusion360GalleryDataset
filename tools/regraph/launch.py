56484c23cefb7a21609aa12d41d6e935d745c79c
Karl D.D. Willis <karl@karlddwillis.com>
2020-08-04 13:28:29 -0700

    Add timeout to launch script

--- tools/regraph/launch.py
+++ tools/regraph/launch.py
@@ -1,6 +1,6 @@
 """
 
-Run Fusion in a loop
+Run Fusion in a loop with a timeout
 Windows only
 
 """
@@ -21,13 +21,46 @@ if COMMON_DIR not in sys.path:
 from launcher import Launcher
 
 
-def launch_loop(launcher):
+def launch_loop(launcher, results_file):
+    """Launch Fusion again and again in a loop"""
     p = launcher.launch()
+    killing = False
+
     while p.poll() is None:
-        time.sleep(1)
-    print(f"Fusion crashed with code {p.returncode}, relaunching...")
-    launch_loop(launcher)
+        if time_out_reached(results_file):
+            if not killing:
+                p.kill()
+            killing = True
+        else:
+            time.sleep(1)
+    
+    if killing:
+        # Update the file to avoid infinite loop
+        results_file.touch()
+        print(f"Fusion killed after timeout, relaunching...")
+    else:
+        print(f"Fusion crashed with code {p.returncode}, relaunching...")
+    launch_loop(launcher, results_file)
+
+
+def time_out_reached(results_file):
+    """Check for a timeout by
+        checking the results file to see if it has been updated"""
+    if not results_file.exists():
+        return False
+    start_time = os.path.getmtime(results_file)
+    time_elapsed =  time.time() - start_time
+    print(f"Time processing current file: {time_elapsed}\r", end="")
+    # Wait for this amount of time before killing
+    time_out_limit = 15 * 60
+    return time_elapsed > time_out_limit
 
 if __name__ == "__main__":
+    current_dir = Path(__file__).resolve().parent
+    output_dir = current_dir / "output"
+    # We use the timestamp of this file to check we haven't timed out
+    # This file is updated at regular intervals when all is working
+    results_file = output_dir / "regraph_results.json"
+    
     launcher = Launcher()
-    launch_loop(launcher)
+    launch_loop(launcher, results_file)
