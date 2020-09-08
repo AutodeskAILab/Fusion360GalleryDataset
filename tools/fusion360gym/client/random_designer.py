6562844b355de13cc5dd4003ae0933aa65869876
rodgerljl@msn.com <rodgerljl@msn.com>
2020-09-07 18:56:04 -0700

    optimize the designer

--- tools/fusion360gym/client/random_designer.py
+++ tools/fusion360gym/client/random_designer.py
@@ -1,242 +1,150 @@
-""""
-A random designer using reconstruct_sketch() and add_extrude()
-to generate 3D models from real-world sketches
-"""
-
-"""
-to-do:
-1. expand sample data
-
-2. force to have at least two steps
-
-3. distribution on sketch plane
-
-4. crash handling
-"""
-
 from pathlib import Path
-import sys
-import os
-from os.path import isfile, join
-import json
-import random
-import time
-import math
+from requests.exceptions import ConnectionError
 import numpy as np
-from fusion_360_client import Fusion360Client
+import math
+import random
 
-# lib for random designer 
-import designer_utilities as designer
+from random_designer_env import RandomDesignerEnv
 
-# Before running ensure the Fusion360Server is running
-# and configured with the same host name and port number
 HOST_NAME = "127.0.0.1"
 PORT_NUMBER = 8080
 
-TOTAL_EPISODES = 1
-
-MIN_AREA = 10
-MAX_AREA = 500
-EXTRUDE_LIMIT = 3
-TRANSLATE_NOISE = 1
-
-RECONSTRUCTION_DATA_PATH = "random_designer_data" # to-do: need to change to training data
+RECONSTRUCTION_DATA_PATH = "d7" 
 GENERATED_DATA_PATH = "generated_design"
 
-# face count distribution
-FACE_COUNTS = [821, 2595, 1950, 1038, 608, 378, 319, 184, 135, 90, 70, 52, 41, 35, 27, 27, 14, 19, 9, 17, 11, 18, 19, 8, 10]
-FACES =  [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100]
-FACE_PROBS = []
-for count in FACE_COUNTS:
-	FACE_PROBS.append(count / sum(FACE_COUNTS))
+TOTAL_EPISODES = 5000
 
-# sketch place distribution
-# to-do: add real distribution later
-SKETCH_PLANE = random.choice(["XY", "XZ", "YZ"])
+MIN_AREA = 10
+MAX_AREA = 2000
+
+EXTRUDE_LIMIT = 3
+TRANSLATE_NOISE = 0
+MAX_NUM_FACES_PER_PROFILE = 20
+MAX_STEPS = 4
 
 def main():
-	# initialize client 
-	client = Fusion360Client(f"http://{HOST_NAME}:{PORT_NUMBER}")
 
-	# assign path
 	current_dir = Path(__file__).resolve().parent
-	data_dir = current_dir / RECONSTRUCTION_DATA_PATH
-	generated_dir = current_dir / GENERATED_DATA_PATH
-	
+	input_dir = current_dir / RECONSTRUCTION_DATA_PATH
+	output_dir = current_dir / GENERATED_DATA_PATH
+
+	random_designer = RandomDesignerEnv(host=HOST_NAME, port=PORT_NUMBER, extrude_limit=EXTRUDE_LIMIT)
+
 	new_body = False
 	episode = 0
 	
 	while episode < TOTAL_EPISODES:
-		client.clear()
 
-		# sample the target number of faces for this design  
-		face_target = np.random.choice(FACES, 1, p=FACE_PROBS)[0]
-		print("target faces: {}".format(face_target))
+		try:
 
-				
-		# pick up a random json file from a folder
-		json_files = [f for f in os.listdir(data_dir) if f.endswith('.json')]
-		json_file_dir = data_dir / random.choice(json_files)
-		# retrieve the json in case we need to investigate it 
-		print("base sketch：{}".format(json_file_dir))
+			current_num_faces = 0
 
-		# load a single json file for debug
-		# json_file_dir = current_dir / "sample_data/demo_01.json"
+			# setup 
+			random_designer.client.clear()
+			target_face, sketch_plane = random_designer.setup_from_distributions()
 
-		# multiple extrudes from one sketch may cause the merged-face issue
-		# to result in regraph fail
-		# json_file_dir = current_dir / "random_designer_data/60295_e0b2e08b_0000.json"
+			# pick up a random json file
+			json_data, json_file_dir = random_designer.select_json(input_dir)
 
-		# NoneType' object has no attribute 'attributes
-		# random_designer_data/146888_62f0c3fe_0001.json
-
-		with open(json_file_dir) as file_handle:
-			json_data = json.load(file_handle)
-		
-		# traverse all the sketches
-		sketches = designer.traverse_sketches(json_data)
-		# skip if the json file doesn't contain sketches 		
-		if len(sketches) == 0:
-			continue
-
-		# pick the sketch that has the largest area
-		sketch, sketch_name, average_area, sketch_area = designer.largest_area(sketches)
-		if sketch is None:
-			continue
-		# print("average area: {}".format(average_area))
-		# print("max area: {}".format(max_area))
-		
-		# filter out designs are too larger 
-		if sketch_area > MAX_AREA or sketch_area < MIN_AREA:
-			print("Invalid area")
-			continue
-
-		# calculate the centroid of the sketch
-		sketch_centroid = designer.calculate_sketch_centroid(sketch)
-		# print("base sketch centroid: {}".format(sketch_centroid))
-		
-		# translate the sketch to the center
-		if SKETCH_PLANE == "XY":
-			translate = {"x": -sketch_centroid["x"], "y": -sketch_centroid["y"], "z": 0}
-		elif SKETCH_PLANE == "XZ":
-			translate = {"x": -sketch_centroid["x"], "y": 0, "z": -sketch_centroid["z"]}
-		elif SKETCH_PLANE == "YZ":
-			translate = {"x": 0, "y": -sketch_centroid["y"], "z": -sketch_centroid["z"]}
-		scale = {"x": 1, "y": 1, "z": 1}		
-
-		# reconsturct the based sketch
-		r = client.reconstruct_sketch(json_data, sketch_name, sketch_plane=SKETCH_PLANE, scale=scale, translate=translate)
-		
-		response_data = r.json()
-		if response_data["status"] == 200:
-			print("sketches are reconstructed")
-		elif response_data["status"] == 500:
-			print(response_data["message"])
-
-		# extrude profiles 
-		if "data" in response_data and "profiles" in response_data["data"]:
-			profiles = response_data["data"]["profiles"]
-			sketch_name = response_data["data"]["sketch_name"]
-
-			average_area = designer.calculate_average_area(profiles)
-			
-			bases_faces = []
-			for profile_id in profiles:
-				if math.ceil(profiles[profile_id]["properties"]["area"]) >= math.ceil(average_area):
-					if not new_body: 
-						r = client.add_extrude(sketch_name, profile_id, 
-												random.uniform(0, EXTRUDE_LIMIT),
-												"NewBodyFeatureOperation")
-						new_body = True
-					else:
-						r = client.add_extrude(sketch_name, profile_id, 
-							random.uniform(0, EXTRUDE_LIMIT),
-							"JoinFeatureOperation")
-
-					response_data = r.json()
-					if response_data["status"] != 500:
-						bases_faces.append(response_data)
-		else:
-			continue
-		
-		# to-do: the way to calculate the current face might be problematic 
-		current_faces = 0
-		for data in bases_faces:
-			current_faces += len(data["data"]["faces"])
-		print("Current faces: {}".format(current_faces))
-
-		# start the sub-sketches 
-		while current_faces < face_target:
-
-			# select the last face (usually the one on the top) to draw sub-sketch
-			data = np.random.choice(bases_faces, 1)[0]
-			faces = data["data"]["faces"]
-			sketch_plane = faces[-1]["face_id"]
-			
-			# pick up a random json file from a folder
-			json_files = [f for f in os.listdir(data_dir) if f.endswith('.json')]
-			json_file_dir = data_dir / random.choice(json_files)
-			# testing json file
-			# json_file_dir = current_dir / "sample_data/Z0HexagonCutJoin_RootComponent.json"
-			
-			with open(json_file_dir) as file_handle:
-				json_data = json.load(file_handle)
 			# retrieve the json in case we need to investigate it 
-			print(json_file_dir)
-			
-			sketches = designer.traverse_sketches(json_data)
+			print("The base sketch is：{}\n".format(json_file_dir))
+
+			# traverse all the sketches from the json data
+			sketches = random_designer.traverse_sketches(json_data)
+			# skip if the json file doesn't contain sketches 		
 			if len(sketches) == 0:
 				continue
-			
-			sketch = np.random.choice(sketches, 1)[0]
-			sketch_name = sketch["name"]   
-			sketch_centroid = designer.calculate_sketch_centroid(sketch)
-			sketch_average_area = designer.calculate_average_area(sketch["profiles"])
-			# print(sketch_centroid)
-			
-			scale = {"x": 1, "y": 1, "z": 1}
-			if(sketch_average_area > average_area * 2):
-				resize_factor = math.ceil(sketch_average_area / average_area)
-				scale =  {"x": 1/resize_factor, "y": 1/resize_factor, "z": 1/resize_factor}
-			translate = {"x": -sketch_centroid["x"] + random.uniform(-TRANSLATE_NOISE, TRANSLATE_NOISE), 
-							"y": -sketch_centroid["y"] + random.uniform(-TRANSLATE_NOISE, TRANSLATE_NOISE), 
-							"z": 0}
-			
-			r = client.reconstruct_sketch(json_data, sketch_name, sketch_plane=sketch_plane, scale=scale, translate=translate)
 
-			# extrude profiles 
-			if "data" in r.json() and "profiles" in r.json()["data"]:
-				response_data = r.json()["data"]
-				profiles = response_data["profiles"]
-				sketch_name = response_data["sketch_name"]
+			# pick the sketch that has the largest area
+			sketch, sketch_name, average_area, sketch_area = random_designer.largest_area(sketches)
+			if sketch is None:
+				continue
+			# print("average area: {}".format(average_area))
+			# print("max area: {}".format(max_area))
+			
+			# filter out designs are too larger 
+			if sketch_area > MAX_AREA or sketch_area < MIN_AREA:
+				print("Invalid area\n")
+				continue
+
+			# calculate the centroid of the sketch
+			sketch_centroid = random_designer.calculate_sketch_centroid(sketch)
+			# print("base sketch centroid: {}".format(sketch_centroid))
+			
+			# translate the sketch to the center
+			if sketch_plane == "XY":
+				translate = {"x": -sketch_centroid["x"], "y": -sketch_centroid["y"], "z": 0}
+			elif sketch_plane == "XZ":
+				translate = {"x": -sketch_centroid["x"], "y": 0, "z": -sketch_centroid["z"]}
+			elif sketch_plane == "YZ":
+				translate = {"x": 0, "y": -sketch_centroid["y"], "z": -sketch_centroid["z"]}
+			scale = {"x": 1, "y": 1, "z": 1}		
+
+			# reconsturct the based sketch
+			r = random_designer.client.reconstruct_sketch(json_data, sketch_name, sketch_plane=sketch_plane, scale=scale, translate=translate)
+			response_data = r.json()
+			if response_data["status"] == 500:
+				print(response_data["message"])
+				continue
+
+			base_faces, num_faces = random_designer.extrude_profiles(response_data)
+			if base_faces is None or num_faces > MAX_NUM_FACES_PER_PROFILE:
+				continue
+			current_num_faces += num_faces
+
+			# start the sub-sketches 
+			steps = 0
+			while current_num_faces < target_face and len(base_faces) > 0 and steps < MAX_STEPS:
+
+				try:
+					sketch_plane = random_designer.select_plane(base_faces)
+				except ValueError:
+					continue
+
+				# pick up a new random json file
+				json_data, json_file_dir = random_designer.select_json(input_dir)
+				print("The sub-sketch is：{}\n".format(json_file_dir))
+
+				sketches = random_designer.traverse_sketches(json_data)
+				if len(sketches) == 0:
+					continue
+			
+				sketch = np.random.choice(sketches, 1)[0]
+				sketch_name = sketch["name"]   
+				sketch_centroid = random_designer.calculate_sketch_centroid(sketch)
+				sketch_average_area = random_designer.calculate_average_area(sketch["profiles"])
+
+				scale = {"x": 1, "y": 1, "z": 1}
+				if(sketch_average_area > average_area * 2):
+					resize_factor = math.ceil(sketch_average_area / average_area)
+					scale =  {"x": 1/resize_factor, "y": 1/resize_factor, "z": 1/resize_factor}
+				translate = {"x": -sketch_centroid["x"] + random.uniform(-TRANSLATE_NOISE, TRANSLATE_NOISE), 
+								"y": -sketch_centroid["y"] + random.uniform(-TRANSLATE_NOISE, TRANSLATE_NOISE), 
+								"z": 0}
 				
-				# extrude all profiles
-				# for profile_id in profiles:
-				#     r = client.add_extrude(sketch_name, profile_id, random.uniform(0, EXTRUDE_LIMIT/2), "JoinFeatureOperation")
+				r = random_designer.client.reconstruct_sketch(json_data, sketch_name, sketch_plane=sketch_plane, scale=scale, translate=translate)
+				response_data = r.json()
+				if response_data["status"] == 500:
+					print(response_data["message"])
 
-				# extrude a random profile
-				profile_id = random.choice(list(profiles.keys()))
-				r = client.add_extrude(sketch_name, profile_id, random.uniform(0, EXTRUDE_LIMIT/2), "JoinFeatureOperation")
+				num_faces = random_designer.extrude_one_profile(response_data)
+				current_num_faces += num_faces
 
-				# update the current faces
-				for data in bases_faces:
-					current_faces += len(data["data"]["faces"])
-				print("Current faces: {}".format(current_faces))
-		
-		# save graph 
-		json_file_dir = generated_dir
-		json_file = str(episode) + ".json"
-		r = client.graph(json_file, json_file_dir, format="PerFace")
-		if r.status_code == 500:
-			print(r.json()["message"])
-		else:
-			# save f3d
-			f3d_file = str(episode) + ".f3d"
-			f3d_file_dir = generated_dir / f3d_file
-			client.brep(f3d_file_dir)
-			episode += 1
+				steps += 1
 
-		print("current episode: {}".format(episode))
+			# save graph and f3d
+			try:
+				success = random_designer.save(output_dir)
+				if success:
+					episode += 1
+			except OSError:
+				random_designer.launch_gym()
+
+		except ConnectionError as ex:
+
+			random_designer.launch_gym()
+			continue
+
 
 if __name__ == "__main__":
 	main() 
---
+++ tools/fusion360gym/client/random_designer_env.py
@@ -0,0 +1,204 @@
+import os
+import random
+import json
+import numpy as np
+import math
+import time
+
+from gym_env import GymEnv
+
+class RandomDesignerEnv(GymEnv):
+
+	def __init__(self, host, port, extrude_limit):
+
+		self.extrude_limit = extrude_limit
+		self.new_body = False
+		self.max_base_profiles = 1
+
+		GymEnv.__init__(self, host=host, port=port)
+
+	# return the sketch that has the largest area
+	def largest_area(self, sketches):
+	    max_area = 0
+	    return_sketch = None
+	    for sketch in sketches:
+	        areas = []
+	        if "profiles" in sketch:
+	            profiles = sketch["profiles"]
+	            for profile in profiles:
+	                areas.append(profiles[profile]["properties"]["area"])
+	            sum_area = sum(areas)
+	            average_area = sum_area / len(areas)
+	            if sum_area > max_area:
+	                max_area = sum_area
+	                return_sketch = sketch
+	    return return_sketch, return_sketch["name"], average_area, max_area
+
+
+	# return the centroid of a sketch
+	def calculate_sketch_centroid(self, sketch):
+	    profiles = sketch["profiles"]
+	    # calcuate the centroid of the sketch
+	    sketch_centroid = {"x": 0, "y": 0, "z": 0}
+	    for profile in profiles:
+	        sketch_centroid["x"] += profiles[profile]["properties"]["centroid"]["x"]
+	        sketch_centroid["y"] += profiles[profile]["properties"]["centroid"]["y"]
+	        sketch_centroid["z"] += profiles[profile]["properties"]["centroid"]["z"]
+	    for key in sketch_centroid:
+	        sketch_centroid[key] /= len(profiles)
+	    return sketch_centroid   
+
+
+	# calculate average area from profiles 
+	def calculate_average_area(self, profiles):
+	    average_area = 0
+	    for profile_id in profiles:
+	        average_area += profiles[profile_id]["properties"]["area"]
+	    return average_area / len(profiles)
+
+
+	# traverse all the sketches
+	def traverse_sketches(self, json_data):
+	    sketches = []
+	    timeline = json_data["timeline"]
+	    entities = json_data["entities"]
+	    for timeline_object in timeline:
+	        entity_uuid = timeline_object["entity"]
+	        entity_index = timeline_object["index"]
+	        entity = entities[entity_uuid]
+	        # we only want sketches with profiles 
+	        if entity["type"] == "Sketch" and "profiles" in entity:
+	            sketches.append(entity)
+	    return sketches
+
+
+	# select a radnom json file from the database    
+	def select_json(self, data_dir):
+		json_files = [f for f in os.listdir(data_dir) if f.endswith('.json')]
+		json_file_dir = data_dir / random.choice(json_files)
+		with open(json_file_dir) as file_handle:
+			json_data = json.load(file_handle)
+		return json_data, json_file_dir
+
+	def setup_from_distributions(self):
+		# face count distribution
+		FACE_COUNTS = [821, 2595, 1950, 1038, 608, 378, 319, 184, 135, 90, 70, 52, 41, 35, 27, 27, 14, 19, 9, 17, 11, 18, 19, 8, 10]
+		FACES =  [4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100]
+		FACE_PROBS = []
+		for count in FACE_COUNTS:
+			FACE_PROBS.append(count / sum(FACE_COUNTS))
+		target_face = np.random.choice(FACES, 1, p=FACE_PROBS)[0]
+
+		# sketch place distribution
+		# to-do: add real distribution later
+		sketch_plane = random.choice(["XY", "XZ", "YZ"])
+
+		return target_face, sketch_plane
+
+	def extrude_profiles(self, response_data):
+		# extrude profiles 
+		if "data" in response_data and "profiles" in response_data["data"]:
+			sketch_name = response_data["data"]["sketch_name"]
+			profiles = response_data["data"]["profiles"]
+			average_area = self.calculate_average_area(profiles)
+			bases_faces = []
+			profile_ids = []
+			
+			for profile_id in profiles:
+				if math.ceil(profiles[profile_id]["properties"]["area"]) >= math.ceil(average_area):
+					profile_ids.append(profile_id)
+			# we only extrude a certain number of profiles 
+			if len(profile_ids) > self.max_base_profiles:
+				profile_ids = random.sample(profile_ids, self.max_base_profiles)
+
+			for profile_id in profile_ids:
+				if not self.new_body: 
+					r = self.client.add_extrude(sketch_name, profile_id, random.uniform(0, self.extrude_limit), "NewBodyFeatureOperation")
+					self.new_body = True
+				else:
+					r = self.client.add_extrude(sketch_name, profile_id, random.uniform(0, self.extrude_limit), "JoinFeatureOperation")
+				response_data = r.json()
+				if response_data["status"] != 500:
+					bases_faces.append(response_data)
+			
+			# to-do: the way to calculate the current face might be problematic 
+			num_faces = 0
+			for data in bases_faces:
+				num_faces += len(data["data"]["faces"])
+
+			return bases_faces, num_faces
+		else:
+			return None, None 
+
+	def extrude_one_profile(self, response_data):
+		# extrude profiles 
+		if "data" in response_data and "profiles" in response_data["data"]:
+			profiles = response_data["data"]["profiles"]
+			sketch_name = response_data["data"]["sketch_name"]
+
+			if len(profiles) == 0:
+				return 0
+
+			average_area = self.calculate_average_area(profiles)
+			
+			profile_ids = []
+			for profile_id in profiles:
+				if math.ceil(profiles[profile_id]["properties"]["area"]) >= math.ceil(average_area):
+					profile_ids.append(profile_id)
+			# we only extrude a certain number of profiles 
+			if len(profile_ids) > self.max_base_profiles:
+				profile_ids = random.sample(profile_ids, self.max_base_profiles)
+
+			
+			profile_id = random.choice(profile_ids)
+			r = self.client.add_extrude(sketch_name, profile_id, random.uniform(0, self.extrude_limit), "JoinFeatureOperation")
+				
+			response_data = r.json()
+			
+			# to-do: the way to calculate the current face might be problematic 
+			num_faces = len(response_data["data"]["faces"])
+
+			return int(num_faces)
+		else:
+			return 0
+
+	def save(self, output_dir):
+		json_file_dir = output_dir
+		json_file = str(math.floor(time.time())) + ".json"
+		r = self.client.graph(json_file, json_file_dir, format="PerFace")
+		if r.status_code == 500:
+			print(r.json()["message"])
+			return False
+		else:
+			# save f3d
+			f3d_file = str(math.floor(time.time())) + ".f3d"
+			f3d_file_dir = output_dir / f3d_file
+			self.client.brep(f3d_file_dir)
+			print("Data generation success!\n")
+			return True
+
+	def select_plane(self, base_faces):
+		# randomly pick an extrude 
+		data = np.random.choice(base_faces, 1)[0]
+		faces = data["data"]["faces"]
+		# pick the start face 
+		# sketch_plane = faces[0]["face_id"]
+		face_id = np.random.choice(len(faces), 1)[0]
+		sketch_plane = faces[face_id]["face_id"]
+		return sketch_plane
+
+	# def select_plane(self, base_faces):
+		
+	# 	# randomly pick an extrude 
+	# 	data = np.random.choice(base_faces, 1)[0]
+	# 	faces = data["data"]["faces"]
+	# 	# pick the start face 
+	# 	# sketch_plane = faces[0]["face_id"]
+	# 	face_id = np.random.choice(len(faces), 1)[0]
+	# 	sketch_plane = faces[face_id]["face_id"]
+
+	# 	return sketch_plane, base_faces
+			
+
+
+
